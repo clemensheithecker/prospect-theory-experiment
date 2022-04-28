66e21c7ba7a14ffb38714d2a62eee8fe31dc899b
Clemens S. Heithecker <clemens@heithecker.dev>
2022-04-28 15:02:27 +0200

    Add handout and investment factors table

--- scripts/data-generation.R
+++ scripts/data-generation.R
@@ -12,11 +12,10 @@ library(tidyverse)
 # A package to convert a data frame into an exportable format
 library(flextable)
 
+library(officer)
+
 
 # Variables ---------------------------------------------------------------
-
-# Number of rounds
-rounds <- 6
 
 # Number of participants (even number)
 n <- 48
@@ -34,148 +33,201 @@ mean_up <- 1.6
 sd_up <- sd_neutral
 
 
-# Create mirroring negative shock
+# Set seed ----------------------------------------------------------------
 
-# Negative shock distribution: mean
-mean_down = mean_neutral - (mean_up - mean_neutral)
-
-# Negative shock distribution: standard deviation
-sd_down = sd_up
+set.seed(8642)
 
 
 # Generate prices ---------------------------------------------------------
 
-generate_price_table <-
+generate_investment_factors_table <-
   function(n,
-           rounds,
            mean_neutral,
            sd_neutral,
            mean_up,
-           sd_up,
-           mean_down,
-           sd_down) {
-    prices <- data.frame(
-      tibble(id = 1:n)
-    )
+           sd_up) {
+    investment_factors <- data.frame(tibble(id = 1:n))
     
-    # Neutral
-    for (round in 1:(rounds / 2)) {
+    # Create mirroring negative shock distribution
+    mean_down = mean_neutral - (mean_up - mean_neutral)
+    sd_down = sd_up
+    
+    # Neutral market
+    for (round in 1:4) {
       round_name <- paste("round_", round, sep = "")
       
-      prices[round_name] = round(
-        rnorm(n, mean = mean_neutral, sd = sd_neutral),
-        digits = 2
-      )
+      investment_factors[round_name] <-
+        round(
+          rnorm(n, mean = mean_neutral, sd = sd_neutral),
+          digits = 2
+        )
     }
     
-    # Volatility shock
-    for (round in (rounds / 2 + 1):(rounds + 1)) {
+    # Market with volatility shock
+    for (round in 5:9) {
       round_name <- paste("round_", round, sep = "")
       
-      # Up
-      prices[1:(n / 2), round_name] = round(
-        rnorm(n / 2, mean = mean_up, sd = sd_up),
-        digits = 2
-      )
+      # Positive shock
+      investment_factors[1:(n / 2), round_name] <-
+        round(
+          rnorm(n / 2, mean = mean_up, sd = sd_up),
+          digits = 2
+        )
       
-      # Down
-      prices[(n / 2 + 1):n, round_name] = round(
-        rnorm(n / 2, mean = mean_down, sd = sd_down),
-        digits = 2
-      )
+      # Negative shock
+      investment_factors[(n / 2 + 1):n, round_name] <-
+        round(
+          rnorm(n / 2, mean = mean_down, sd = sd_down),
+          digits = 2
+        )
     }
     
-    return(prices)
-}
+    return(investment_factors)
+  }
 
 
-prices <- generate_price_table(
+investment_factors <- generate_investment_factors_table(
   n = n,
-  rounds = rounds,
   mean_neutral = mean_neutral,
   sd_neutral = sd_neutral,
   mean_up = mean_up,
-  sd_up = sd_up,
-  mean_down = mean_down,
-  sd_down = sd_down
+  sd_up = sd_up
 )
 
-str(prices)
+head(investment_factors)
 
 
 # Export table to word ----------------------------------------------------
 
-flextable <- flextable(data = prices) %>%
+investment_factors_word <- flextable(investment_factors) %>%
   add_header_row(
-    values = c("", "Returns"),
-    colwidths = c(1, 7)
+    values = c("", "Investment Factors"),
+    colwidths = c(1, 9)
   ) %>%
-  theme_box %>%
-  autofit %>%
-  align(align = "center", part = "all")
+  theme_vanilla %>%
+  fontsize(size = 10) %>%
+  vline(
+    j = c('round_4', 'round_8'),
+    border = officer::fp_border(),
+    part = "all"
+  ) %>%
+  align(align = "center", part = "all") %>%
+  set_table_properties(width = 1, layout = "autofit")
 
-flextable
-
-save_as_docx(flextable, path = "../documents/prices.docx")
-
-
-x# Distribution visualizations ---------------------------------------------
-
-# Neutral
-
-neutral <- as.vector(
-  c(
-    prices$round_1,
-    prices$round_2,
-    prices$round_3
+save_as_docx(
+  investment_factors_word,
+  path = "../documents/investment-factors.docx",
+  pr_section = officer::prop_section(
+    page_margins = officer::page_mar(
+      bottom = 1,
+      top = 1,
+      right = 0.75,
+      left = 0.75,
+      header = 0,
+      footer = 0,
+      gutter = 0
+    )
   )
 )
 
-ggplot(mapping = aes(x = neutral)) +
-  geom_histogram(bins = 8) +
-  labs(
-    title = "Distribution of Neutral Market State Returns",
-    x = "Returns",
-    y = "Frequency"
-  ) +
-  theme_minimal()
 
-# Up
+# Save data as RData file -------------------------------------------------
 
-up <- as.vector(
+save(investment_factors, file = "../data/investment-factors.RData")
+
+
+# Distribution visualizations ---------------------------------------------
+
+# Neutral market state
+
+investment_factors_neutral <- as.vector(
   c(
-    prices[1:(n / 2), "round_4"],
-    prices[1:(n / 2), "round_5"],
-    prices[1:(n / 2), "round_6"],
-    prices[1:(n / 2), "round_7"]
+    investment_factors$round_1,
+    investment_factors$round_2,
+    investment_factors$round_3,
+    investment_factors$round_4
   )
 )
 
-ggplot(mapping = aes(x = up)) +
-  geom_histogram(bins = 8) +
-  labs(
-    title = "Distribution of Positive Market Shock Returns",
-    x = "Returns",
-    y = "Frequency"
-  ) +
-  theme_minimal()
+investment_factors_neutral_distribution <-
+  ggplot(mapping = aes(x = investment_factors_neutral)) +
+    geom_histogram(bins = 8) +
+    labs(
+      title = "Distribution of Investment Factors in Neutral Market",
+      x = "Investment Factor",
+      y = "Frequency"
+    ) +
+    theme_minimal()
 
-# Down
+ggsave(
+  plot = investment_factors_neutral_distribution,
+  "../figures/investment-factors-neutral-market-distribution.png",
+  width = 16,
+  height = 10,
+  units = "cm",
+  bg = "white"
+)
 
-down <- as.vector(
+
+# Positive market shock
+
+investment_factors_up <- as.vector(
   c(
-    prices[(n / 2 + 1):n, "round_4"],
-    prices[(n / 2 + 1):n, "round_5"],
-    prices[(n / 2 + 1):n, "round_6"],
-    prices[(n / 2 + 1):n, "round_7"]
+    investment_factors[1:(n / 2), "round_5"],
+    investment_factors[1:(n / 2), "round_6"],
+    investment_factors[1:(n / 2), "round_7"],
+    investment_factors[1:(n / 2), "round_8"],
+    investment_factors[1:(n / 2), "round_9"]
   )
 )
 
-ggplot(mapping = aes(x = down)) +
-  geom_histogram(bins = 8) +
-  labs(
-    title = "Distribution of Negative Market Shock Returns",
-    x = "Returns",
-    y = "Frequency"
-  ) +
-  theme_minimal()
+investment_factors_up_distribution <-
+  ggplot(mapping = aes(x = investment_factors_up)) +
+    geom_histogram(bins = 8) +
+    labs(
+      title = "Distribution of Investment Factors for Positive Market Shock",
+      x = "Investment Factor",
+      y = "Frequency"
+    ) +
+    theme_minimal()
+
+ggsave(
+  plot = investment_factors_up_distribution,
+  "../figures/investment-factors-positive-shock-distribution.png",
+  width = 16,
+  height = 10,
+  units = "cm",
+  bg = "white"
+)
+
+
+# Negative market shock
+
+investment_factors_down <- as.vector(
+  c(
+    investment_factors[(n / 2 + 1):n, "round_5"],
+    investment_factors[(n / 2 + 1):n, "round_6"],
+    investment_factors[(n / 2 + 1):n, "round_7"],
+    investment_factors[(n / 2 + 1):n, "round_8"],
+    investment_factors[(n / 2 + 1):n, "round_9"]
+  )
+)
+
+investment_factors_down_distribution <-
+  ggplot(mapping = aes(x = investment_factors_down)) +
+    geom_histogram(bins = 8) +
+    labs(
+      title = "Distribution of Investment Factors for Negative Market Shock",
+      x = "Investment Factor",
+      y = "Frequency"
+    ) +
+    theme_minimal()
+
+ggsave(
+  plot = investment_factors_down_distribution,
+  "../figures/investment-factors-negative-shock-distribution.png",
+  width = 16,
+  height = 10,
+  units = "cm",
+  bg = "white"
+)
